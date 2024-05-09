f720f19407135e3b759835ed5163e0d70ccfdb18
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-05-09 10:44:32 +0200

    first commit react designs

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { BiTrash } from "react-icons/bi";
+import Modal from "./Modal";
+import { useState } from "react";
+
+export default function App() {
+
+  const [ open, setOpen ] = useState(false);
+
+  return (
+    <main className='App'>
+        <button className='btn btn-danger' onClick={() => setOpen(true)}>
+            <BiTrash /> Delete
+        </button>
+
+        <Modal open={open} onClose={() => setOpen(false)}> 
+        <div className="text-center w-56">
+          <BiTrash size={56} className="mx-auto text-red-300" />
+            <div className="mx-auto my-4 w-48">
+              <h3 className="text-lg font-black text-gray-800">
+                Confirm Delete
+              </h3>
+              <p className="text-sm text-gray-500">
+                Are you sure you want to delete this item?
+              </p>
+            </div>
+            <div className="flex gap-4">
+              <button className="btn btn-danger w-full">Delete</button>
+              <button
+              onClick={() => setOpen(false)} 
+              className="btn btn-light w-full">Cancel</button>
+            </div>
+
+        </div>
+        </Modal>
+    </main>
+  )
+}
